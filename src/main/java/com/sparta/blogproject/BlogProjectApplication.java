fd3b9646fdba58bdd4c5e6655716c5b18adb41d9
2h5i <dev.2h5i@gmail.com>
2022-12-30 12:04:45 +0900

    테스트용 커밋입니다.

--- src/main/java/com/sparta/blogproject/BlogProjectApplication.java
+++ src/main/java/com/sparta/blogproject/BlogProjectApplication.java
@@ -8,6 +8,7 @@ public class BlogProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogProjectApplication.class, args);
+        System.out.println("test");
     }
 
 }
