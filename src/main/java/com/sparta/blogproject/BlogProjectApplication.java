85ba3a7ce4f1afd78c3266c78be34b8415262852
2h5i <dev.2h5i@gmail.com>
2022-12-30 09:21:36 +0900

    initial commit, 레파지토리 생성

---
+++ src/main/java/com/sparta/blogproject/BlogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.blogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogProjectApplication.class, args);
+    }
+
+}
