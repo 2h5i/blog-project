a72e2301ebd459ed7d86e9b193af98b091c7d1f6
jeong hyewon <wonizizi89@naver.com>
2022-12-30 21:11:58 +0900

    충돌 해결

--- src/main/java/com/sparta/blogproject/user/controller/UserController.java
+++ src/main/java/com/sparta/blogproject/user/controller/UserController.java
@@ -23,19 +23,13 @@ public class UserController {
     //    - 1. 회원 가입 API
 //    - username, password를 Client에서 전달받기
 //    - DB에 중복된 username이 없다면 회원을 저장하고 Client 로 성공했다는 메시지, 상태코드 반환하기
-<<<<<<< Updated upstream
-//    - 회원 권한 부여하기 (ADMIN, USER) - ADMIN 회원은 모든 게시글 수정 / 삭제 가능
-    @PostMapping("/signup")
-    public ResponseStatusDto signup(@RequestBody @Valid SignupRequest signupRequest) {
-        return userService.signup(signupRequest);
-    }
-=======
+
 ////    - 회원 권한 부여하기 (ADMIN, USER) - ADMIN 회원은 모든 게시글 수정 / 삭제 가능
 //    @PostMapping("/signup")
 //    public ResponseStatusDto signup(@RequestBody @Valid SignupRequest signupRequest) {
 //        return userService.signup(signupRequest);
 //    }
->>>>>>> Stashed changes
+
 
 
 //- 2. 로그인 API
@@ -44,14 +38,7 @@ public class UserController {
 //    - 로그인 성공 시, 로그인에 성공한 유저의 정보와 JWT를 활용하여 토큰을 발급하고,
 //    발급한 토큰을 Header에 추가하고 성공했다는 메시지, 상태 코드 와 함께 Client에
 
-<<<<<<< Updated upstream
-    @PostMapping("/login")
-    public ResponseStatusDto login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
-        return userService.login(loginRequest, response);
-        //또는 return ResponseEntity.status(HttpStatus.OK).body(userService.login(loginRequestDto,response)); 간단히 표시가능
 
-    }
-=======
 //    @PostMapping("/login")
 //    public ResponseStatusDto login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
 //        return userService.login(loginRequest, response);
@@ -59,6 +46,6 @@ public class UserController {
 //
 //    }
 
->>>>>>> Stashed changes
+
 
 }
