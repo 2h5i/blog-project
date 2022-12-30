4a5350f083e7b9401a9722bfa957beea4826d27e
jeong hyewon <wonizizi89@naver.com>
2022-12-30 15:20:54 +0900

    TImeStamped

--- src/main/java/com/sparta/blogproject/common/TimeStamped.java
+++ src/main/java/com/sparta/blogproject/common/TimeStamped.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class TimeStamped {
-
+        //
         @CreatedDate
         private LocalDateTime createdAt;
 
