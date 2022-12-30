e46b9b0dbfdb4205d05cd8678e53ff42f41c178c
hong-seungyeop <milnil0228@gmail.com>
2022-12-30 12:38:29 +0900

    4-test

--- src/main/java/com/sparta/blogproject/BlogProjectApplication.java
+++ src/main/java/com/sparta/blogproject/BlogProjectApplication.java
@@ -8,7 +8,7 @@ public class BlogProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogProjectApplication.class, args);
-        System.out.println("test");
+        System.out.println("4-test");
     }
 
 }
