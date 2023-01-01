0af96bfed55ca801b9fbadf6a8ecfb7366783825
jeong hyewon <wonizizi89@naver.com>
2023-01-01 17:22:52 +0900

    User  회원가입 , 로그인 구현

--- src/main/java/com/sparta/blogproject/user/entity/User.java
+++ src/main/java/com/sparta/blogproject/user/entity/User.java
@@ -1,6 +1,7 @@
 package com.sparta.blogproject.user.entity;
 
 import com.sparta.blogproject.post.entity.Post;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -8,12 +9,13 @@ import javax.xml.stream.events.Comment;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @Entity
 @NoArgsConstructor
 public class User {
 
     @Id
-    @GeneratedValue(strategy  = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
@@ -22,22 +24,23 @@ public class User {
     @Column(nullable = false, unique = true)
     private String password;
 
-//    @Column(nullable = false)
-//    private String email;
-//테스트
+    @Column(nullable = false)
+    private String email;
+
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     private UserRoleEnum userRole;
 
-    @OneToMany(fetch = FetchType.LAZY,mappedBy = "user")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
     List<Post> postList = new ArrayList<>();
 
-    @OneToMany(fetch = FetchType.LAZY,mappedBy = "user")
-    List<Comment> commentList = new ArrayList<>();
+//    @OneToMany(fetch = FetchType.LAZY,mappedBy = "user")
+//    List<Comment> commentList = new ArrayList<>();
 
-    public User(String username, String password, UserRoleEnum userRole) {
+    public User(String username, String password, String email, UserRoleEnum userRole) {
         this.username = username;
         this.password = password;
+        this.email = email;
         this.userRole = userRole;
     }
 }
--- src/main/java/com/sparta/blogproject/user/service/UserService.java
+++ src/main/java/com/sparta/blogproject/user/service/UserService.java
@@ -1,12 +1,77 @@
 package com.sparta.blogproject.user.service;
 
+
+import com.sparta.blogproject.common.jwt.JwtUtil;
+import com.sparta.blogproject.user.dto.LoginRequest;
+import com.sparta.blogproject.user.dto.ResponseStatusDto;
+import com.sparta.blogproject.user.dto.SignupRequest;
+import com.sparta.blogproject.user.dto.StatusEnum;
+import com.sparta.blogproject.user.entity.User;
+import com.sparta.blogproject.user.entity.UserRoleEnum;
+import com.sparta.blogproject.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.util.Optional;
 
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    // ADMIN_TOKEN
+    private static final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+
+    @Transactional
+    public ResponseStatusDto signup(@Valid SignupRequest signupRequest) {
+        String username = signupRequest.getUsername();
+        String password = signupRequest.getPassword();
+
+        //회원중복확인
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 username 입니다.");//statuCode:400 에러메시지와 같이 반환
+        }
+        String email = signupRequest.getEmail();
+
+        //사용자 Role 확인
+        UserRoleEnum role = UserRoleEnum.USER;
+        if (signupRequest.isAdmin()) {
+            if (!signupRequest.getAdminToken().equals(ADMIN_TOKEN)) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능 합니다.");
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        User user = new User(username, password, email, role);
+
+        userRepository.save(user);
+        return new ResponseStatusDto(StatusEnum.LOGIN_SUCCESS);
+
+    }
+
+    @Transactional // readOnly= true  하면 에러남
+    public ResponseStatusDto login(LoginRequest loginRequest, HttpServletResponse response) {
+        String username = loginRequest.getUsername();
+        String password = loginRequest.getPassword();
+
+        // 사용자 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+        //비밀번호 확인
+        if (!user.getPassword().equals(password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다. ");
+        }
+    //    response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(),user.getUserRole()));
+
+
+        return new ResponseStatusDto(StatusEnum.LOGIN_SUCCESS);
+    }
 
 }
