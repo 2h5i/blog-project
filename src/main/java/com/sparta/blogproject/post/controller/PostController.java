accd6b5ad3f027e61a8191d0fd573a072194220a
milnil0228 <milnil0228@gmail.com>
2023-01-02 12:01:19 +0900

    Post 기능 구현

--- src/main/java/com/sparta/blogproject/post/controller/PostController.java
+++ src/main/java/com/sparta/blogproject/post/controller/PostController.java
@@ -2,34 +2,45 @@ package com.sparta.blogproject.post.controller;
 
 import com.sparta.blogproject.common.jwt.JwtUtil;
 import com.sparta.blogproject.post.dto.PostRequestDto;
+import com.sparta.blogproject.post.dto.PostResponseDto;
 import com.sparta.blogproject.post.entity.Post;
 import com.sparta.blogproject.post.service.PostService;
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
-    private final JwtUtil jwtUtil;
 
     @PostMapping("/api/posts")
     public ResponseEntity createPost(@RequestBody PostRequestDto postRequestDto) {
-        String username = postRequestDto.getUser().getUsername;
         postService.createPost(postRequestDto);
-        Post post = new Post(postRequestDto, user);
         return ResponseEntity.ok("게시글 작성 완료");
     }
 
     @GetMapping("/api/posts")
     public List<PostResponseDto> getPosts() {
         return postService.getPosts();
+    }
+
+    @GetMapping("/api/post/{id}")
+    public PostResponseDto getPost(@PathVariable Long id) {
+        return postService.getPost(id);
+    }
+
+    @PutMapping("/api/posts/{id}")
+    public ResponseEntity updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto) {
+        postService.update(id, postRequestDto);
+        return ResponseEntity.ok("게시글 수정 완료");
+    }
+
+    @DeleteMapping("/api/posts/{id}")
+    public ResponseEntity deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+        return ResponseEntity.ok("게시글 삭제 완료");
     }
 }
--- src/main/java/com/sparta/blogproject/post/service/PostService.java
+++ src/main/java/com/sparta/blogproject/post/service/PostService.java
@@ -1,23 +1,64 @@
 package com.sparta.blogproject.post.service;
 
 import com.sparta.blogproject.post.dto.PostRequestDto;
+import com.sparta.blogproject.post.dto.PostResponseDto;
 import com.sparta.blogproject.post.entity.Post;
 import com.sparta.blogproject.post.repository.PostRepository;
-import io.jsonwebtoken.Claims;
+import com.sparta.blogproject.user.entity.User;
+import com.sparta.blogproject.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostRepository postRepository;
-    // TODO: UserRepository 선언
+    private final UserRepository userRepository;
 
+    @Transactional
+    public void createPost(PostRequestDto postRequestDto) {
+        String username = postRequestDto.getUser().getUsername();
 
-    public void createPost(PostRequestDto postRequestDto, Claims claims) {
-        // TODO: UserRepository 생성 후 findByUsername
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("사용자가 존재하지 않습니다.")
+        );
 
-        // TODO: post에 postRequestDtor, user 받아서 저장하기
+        Post post = new Post(postRequestDto, user);
+        postRepository.save(post);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponseDto> getPosts() {
+        return postRepository.findAllByOrderByModifiedAtDesc().stream().map(PostResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public PostResponseDto getPost(Long id) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("게시글이 존재하지 않습니다.")
+        );
+        return new PostResponseDto(post);
+    }
+
+    public void update(Long id, PostRequestDto postRequestDto) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("게시글이 존재하지 않습니다.")
+        );
+
+        post.update(postRequestDto);
+        postRepository.save(post);
+    }
+
+    public void deletePost(Long id) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("게시글이 존재하지 않습니다.")
+        );
+
+        postRepository.delete(post);
     }
 }
