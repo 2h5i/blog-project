7c70ea3f32ca5fe12cb893c9d02f0170f8911e37
2h5i <dev.2h5i@gmail.com>
2023-01-02 15:02:53 +0900

    Security prePostEnable true 처리

--- src/main/java/com/sparta/blogproject/common/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/blogproject/common/config/WebSecurityConfig.java
@@ -21,7 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true)
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurityConfig {
 
     private final JwtUtil jwtUtil;
