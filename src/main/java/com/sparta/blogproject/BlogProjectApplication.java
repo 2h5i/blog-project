2eb59263ee4dd6a834819bba64714eb6a77c5c03
khakyy <khakyy@naver.com>
2023-01-02 15:56:22 +0900

    ,

--- src/main/java/com/sparta/blogproject/BlogProjectApplication.java
+++ src/main/java/com/sparta/blogproject/BlogProjectApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class BlogProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BlogProjectApplication.class, args);
     }
