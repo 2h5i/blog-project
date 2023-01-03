25d2e688a882464b20f0516bcf5fd2ef92acfa34
khakyy <khakyy@naver.com>
2023-01-03 10:33:13 +0900

    Comment Post 연결.ver3

--- src/main/java/com/sparta/blogproject/post/entity/com.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.blogproject.post.entity;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "com_sparta_blogproject_like_entity_comment")
-public class com.sparta.blogproject.like.entity.Comment {
-}
